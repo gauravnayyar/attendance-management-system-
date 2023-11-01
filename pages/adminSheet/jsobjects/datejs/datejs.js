ea212ffabb4c68c4ced269150ea4f34158cb5962
love sachdev <love.sachdev@gmail.com>
2023-11-01 07:20:52 +0000

    LS- Added the group column

--- pages/adminSheet/jsobjects/datejs/datejs.js
+++ pages/adminSheet/jsobjects/datejs/datejs.js
@@ -16,27 +16,27 @@ export default {
 		this.dataFetchFromMain =[]
 
 	},
-removeDuplicates(data) { 
-const uniqueData = [];
+	removeDuplicates(data) { 
+		const uniqueData = [];
 
-for (let i = 0; i < data.length; i++) {
-  let isDuplicate = false;
-  for (let j = 0; j < i; j++) {
-    if (
-      data[i].BadgeNumber === data[j].BadgeNumber &&
-      data[i].FromDate === data[j].FromDate &&
-      data[i].ToDate === data[j].ToDate
-    ) {
-      isDuplicate = true;
-      break;
-    }
-  }
-  if (!isDuplicate) {
-    uniqueData.push(data[i]);
-  }
-}
-  return uniqueData;
-     },
+		for (let i = 0; i < data.length; i++) {
+			let isDuplicate = false;
+			for (let j = 0; j < i; j++) {
+				if (
+					data[i].BadgeNumber === data[j].BadgeNumber &&
+					data[i].FromDate === data[j].FromDate &&
+					data[i].ToDate === data[j].ToDate
+				) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (!isDuplicate) {
+				uniqueData.push(data[i]);
+			}
+		}
+		return uniqueData;
+	},
 	getBadgeNumberFromAdmin(){
 		adminReport.run()
 		this.dataFetchFromMain =[];
@@ -44,48 +44,48 @@ for (let i = 0; i < data.length; i++) {
 		let reportdata = this.removeDuplicates(adminReport.data)
 		console.log(reportdata)
 		reportdata.forEach((ele) => {
-	if(ele.SewadarType === 'OPEN'){
-					let person = {
-						sno:sn+1,
-						name: ele.BadgeNumber,
-						BadgeNumber: ele.SewadarType,
-						phoneAllowed: ele.PhoneAllowed,
-						vehicleAllowed: ele.VehicleAllowed,
-						fromDate: ele.FromDate,
-						toDate: ele.ToDate,
-						mobileVaccinationNo: '',
-						firstDose: '',
-						secondDose: ''
+			if(ele.SewadarType === 'OPEN'){
+				let person = {
+					sno:sn+1,
+					name: ele.BadgeNumber,
+					BadgeNumber: ele.SewadarType,
+					phoneAllowed: ele.PhoneAllowed,
+					vehicleAllowed: ele.VehicleAllowed,
+					fromDate: ele.FromDate,
+					toDate: ele.ToDate,
+					mobileVaccinationNo: '',
+					firstDose: '',
+					secondDose: ''
 
-					};
+				};
 
-					this.dataFetchFromMain.push(person);
-					sn =sn+1;
-				}else{
-			getMainDataByBadgeId.data.forEach((element)=>{
-			
-						if(ele.BadgeNumber === element.BadgeNumber){
-					let person = {
-						sno:sn+1,
-						name: element.Name,
-						BadgeNumber: element.BadgeNumber,
-						phoneAllowed: ele.PhoneAllowed,
-						vehicleAllowed: ele.VehicleAllowed,
-						fromDate: ele.FromDate,
-						toDate: ele.ToDate,
-						mobileVaccinationNo: element.MobileVaccinationNo,
-						firstDose: element["Vaccination 1st dose"],
-						secondDose: element["Vaccination 2nd dose"]
+				this.dataFetchFromMain.push(person);
+				sn =sn+1;
+			}else{
+				getMainDataByBadgeId.data.forEach((element)=>{
 
-					};
+					if(ele.BadgeNumber === element.BadgeNumber){
+						let person = {
+							sno:sn+1,
+							name: element.Name,
+							BadgeNumber: element.BadgeNumber,
+							phoneAllowed: ele.PhoneAllowed,
+							vehicleAllowed: ele.VehicleAllowed,
+							fromDate: ele.FromDate,
+							toDate: ele.ToDate,
+							mobileVaccinationNo: element.MobileVaccinationNo,
+							firstDose: element["Vaccination 1st dose"],
+							secondDose: element["Vaccination 2nd dose"],
+							Group: element.Group
+						};
 
-					this.dataFetchFromMain.push(person);
-					sn =sn+1;
-				}
-				
-			
-			})
-				}
+						this.dataFetchFromMain.push(person);
+						sn =sn+1;
+					}
+
+
+				})
+			}
 		})
 		return this.dataFetchFromMain;
 	},
